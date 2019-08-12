71486584e7b88bdb2dbe096833e048fce2adf207
Dayton Nolan <me@daytonnolan.com>
2019-08-12 16:16:34 -0500

    Add package info and license file for publishing to hex

--- mix.exs
+++ mix.exs
@@ -4,10 +4,27 @@ defmodule EctoResource.MixProject do
   def project do
     [
       app: :ecto_resource,
-      version: "0.1.0",
+      deps: deps(),
+      description: description(),
       elixir: "~> 1.8",
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      version: "0.1.0"
+    ]
+  end
+
+  defp description do
+    """
+    A simple module to clear up the boilerplate of CRUD resources in Phoenix context files.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Dayton Nolan"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/daytonn/ecto_resource"}
     ]
   end
 
@@ -22,6 +39,7 @@ defmodule EctoResource.MixProject do
   defp deps do
     [
       {:ecto_sql, "~> 3.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:mox, "~> 0.5.0", only: :test},
       {:postgrex, ">= 0.0.0"}
     ]
