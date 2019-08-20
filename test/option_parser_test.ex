fe6c1b14cf458320e0d3a5384463502da3f26455
Dayton Nolan <me@daytonnolan.com>
2019-08-20 09:19:49 -0500

    Add test for bang functions

--- test/option_parser_test.ex
+++ test/option_parser_test.ex
@@ -153,5 +153,55 @@ defmodule EctoResource.OptionParserTest do
                }
              }
     end
+
+    test "when given the except atom and a list of bang functions" do
+      assert OptionParser.parse("suffix", except: [:create!, :delete!, :get!, :update!]) == %{
+               all: %{
+                 name: :all_suffixes,
+                 description: "all_suffixes/1"
+               },
+               change: %{
+                 name: :change_suffix,
+                 description: "change_suffix/1"
+               },
+               create: %{
+                 name: :create_suffix,
+                 description: "create_suffix/1"
+               },
+               delete: %{
+                 name: :delete_suffix,
+                 description: "delete_suffix/1"
+               },
+               get: %{
+                 name: :get_suffix,
+                 description: "get_suffix/2"
+               },
+               update: %{
+                 name: :update_suffix,
+                 description: "update_suffix/2"
+               }
+             }
+    end
+
+    test "when given the only atom and a list of bang functions" do
+      assert OptionParser.parse("suffix", only: [:create!, :delete!, :get!, :update!]) == %{
+               create!: %{
+                 name: :create_suffix!,
+                 description: "create_suffix!/1"
+               },
+               delete!: %{
+                 name: :delete_suffix!,
+                 description: "delete_suffix!/1"
+               },
+               get!: %{
+                 name: :get_suffix!,
+                 description: "get_suffix!/2"
+               },
+               update!: %{
+                 name: :update_suffix!,
+                 description: "update_suffix!/2"
+               }
+             }
+    end
   end
 end
