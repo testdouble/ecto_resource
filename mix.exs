95546ad0f88f463e1a1ab1dfa3bea48eda89d716
Dayton Nolan <me@daytonnolan.com>
2019-08-21 14:24:57 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule EctoResource.MixProject do
       elixir: "~> 1.8",
       package: package(),
       start_permanent: Mix.env() == :prod,
-      version: "1.0.2"
+      version: "1.0.3"
     ]
   end
 
