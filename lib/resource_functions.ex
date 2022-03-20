2e0de4bc75810589ddac0b661bb6e012048eabef
Joseph Lozano <git@josephlozano.xyz>
2022-03-20 17:09:01 -0400

    where filter on all_ functions

--- lib/resource_functions.ex
+++ lib/resource_functions.ex
@@ -106,10 +106,12 @@ defmodule EctoResource.ResourceFunctions do
   def all(repo, schema, options \\ []) do
     preloads = Keyword.get(options, :preloads, [])
     order_by = Keyword.get(options, :order_by, [])
+    where = Keyword.get(options, :where, [])
 
     schema
     |> preload(^preloads)
     |> order_by(^order_by)
+    |> where(^where)
     |> repo.all([])
   end
 
