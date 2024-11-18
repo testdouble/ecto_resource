27777d36574ba6193c40a5573b5a92158ab75680
jakeprem <jakeprem@users.noreply.github.com>
2024-11-18 13:01:14 -0500

    Support limit and offset in `all_` functions (#31)
    Branched off of #30 since I'm using both changes, but this could easily
    be pointed at main.
    
    Does two things:
    1. Refactors the option parsing in `all` to use a reduce. This makes it
    a bit easier to options that don't have an easy default (like `where:
    []`) since it will only apply options that are present.
        2. Adds support for `:limit` and `:offset` options.

--- lib/resource_functions.ex
+++ lib/resource_functions.ex
@@ -104,14 +104,16 @@ defmodule EctoResource.ResourceFunctions do
   @spec all(Ecto.Repo.t(), module, term()) :: list(Ecto.Schema.t())
 
   def all(repo, schema, options \\ []) do
-    preloads = Keyword.get(options, :preloads, [])
-    order_by = Keyword.get(options, :order_by, [])
-    where = Keyword.get(options, :where, [])
-
-    schema
-    |> preload(^preloads)
-    |> order_by(^order_by)
-    |> where(^where)
+    options
+    |> Keyword.take([:preloads, :order_by, :where, :limit, :offset])
+    |> Enum.reduce(schema, fn
+      {:preloads, preloads}, query -> preload(query, ^preloads)
+      {:order_by, order_by}, query -> order_by(query, ^order_by)
+      {:where, where}, query -> where(query, ^where)
+      {:limit, limit}, query -> limit(query, ^limit)
+      {:offset, offset}, query -> offset(query, ^offset)
+      _, query -> query
+    end)
     |> repo.all([])
   end
 
